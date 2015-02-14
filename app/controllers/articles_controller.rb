a03a47ad372bfc913cad6b4189feffb40a169ea3
Karthik Rajan <rkarthi@amazon.com>
2015-02-13 22:31:31 -0800

    Minor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,22 @@ class ArticlesController < ApplicationController
     def new
         # New action for Articles
     end
+    
+    # Create action
+    def create
+        # inspect
+        render plain: params[:article].inspect 
+    end
+
+    def read
+
+    end
+
+    def update
+
+    end
+
+    def destory
+
+    end
 end
